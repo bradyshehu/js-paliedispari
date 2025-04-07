7519ef35c3fad0665d626951b95f748937ed5888
bradyshehu <shehu.brady@gmail.com>
2025-04-07 16:36:15 +0200

    create function

--- Palidroma/js/main.js
+++ Palidroma/js/main.js
@@ -2,4 +2,28 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-const word = "parola";
+const parola = "parola";
+const wordPalindrome = "anna";
+console.log(parola);
+console.log(wordPalindrome);
+
+let reversedWord = [];
+let isWordPalindrome;
+function palindromeCheck(word) {
+  const wordArray = word.split("");
+  console.log(wordArray);
+  reversedWord = [];
+  for (let i = 0; i < word.length; i++) {
+    const currentLetter = word[i];
+    reversedWord.unshift(currentLetter);
+    if (word[i] === reversedWord[i]) {
+      isWordPalindrome = true;
+    } else {
+      isWordPalindrome = false;
+    }
+  }
+  console.log(reversedWord);
+  return isWordPalindrome;
+}
+palindromeCheck(parola);
+console.log(isWordPalindrome);
